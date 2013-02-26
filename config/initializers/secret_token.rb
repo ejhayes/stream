84a34da92933c9f884f14880b7e0749d60230d20
Eric Hayes <eric@inflection.com>
2013-02-25 18:55:13 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stream::Application.config.secret_token = '049abb5f304b2b98eff3ccca188c62cd8fcbdfeea14fca04cd69b6fe071ff9a74fa40edf75449c3dcfeb5c9ef29e66a5b39944f0d9e4b992a895dbf117be42d9'
